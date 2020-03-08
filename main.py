1b09a047d7200c4cd086fdd1f2d489d6e1878934
chenjd <chen.jiadong@outlook.com>
2020-03-08 21:20:33 +1300

    import pydot to generate png

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+import os
+import sys
+import pydot
 from pylint import run_pyreverse
 
-run_pyreverse()
+
+class PyUML:
+    def __init__(self):
+        print('pyuml start')
+
+    def run(self, args):
+        run_pyreverse()
+        self._render_with_pydot("classes.dot", "utf8")
+
+    def _render_with_pydot(self, filename, encoding):
+        graphs = pydot.graph_from_dot_file(filename, encoding=encoding)
+        for g in graphs:
+            path = os.path.abspath(os.getcwd()) + '/test.png'
+            g.write_png(path)
+
+
+def run_pyuml():
+    """run pyuml"""
+    PyUML().run(sys.argv[1:])
+
+
+run_pyuml()
