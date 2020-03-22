f60ed13e3c98e62547ff228111fbeb9c7a8aecbb
chenjd <chen.jiadong@outlook.com>
2020-03-22 21:37:15 +1300

    add test case

--- test/pyuml_unittest.py
+++ test/pyuml_unittest.py
@@ -10,6 +10,7 @@ from core.parser import ClassParser
 from core.writer import DotWriter
 
 TESTS_DIR = 'test_py_code'
+TESTS_DIR2 = 'test_dot_code'
 
 
 class TestPyUmlAPI(unittest.TestCase):
@@ -36,7 +37,7 @@ class TestPyUmlAPI(unittest.TestCase):
     def test_loader_load_from_directory_find_out_py_file(self):
         loader = Loader()
         ret = loader.load_from_file_or_directory(TESTS_DIR)
-        expected_files_count = 3
+        expected_files_count = 8
         self.assertEqual(expected_files_count, len(ret))
 
     def test_loader_load_from_directory_not_exist_throw_exception(self):
@@ -73,7 +74,6 @@ class TestPyUmlAPI(unittest.TestCase):
         self.assertEqual(expected_class_name, class_parser.classes_list[0].name)
         self.assertEqual(expected_memober_count, len(class_parser.classes_list[0].members))
         self.assertEqual(expected_methods_count, len(class_parser.classes_list[0].methods))
-
 
     def test_parser_parse_class_with_constructor_data_members(self):
         local_dir = os.path.join(TESTS_DIR, "py_src_code_class_with_data.py")
@@ -121,8 +121,101 @@ class TestPyUmlAPI(unittest.TestCase):
             elif cla.name is "MyClassTwo":
                 self.assertEqual(2, len(cla.parents))
 
+    def test_writter_write_dot_only_class_define(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_only_class_def.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        tree = ast3.parse(code_string_list[0])
+        class_parser = ClassParser()
+        class_parser.visit(tree)
+
+        writer = DotWriter()
+        result_dot = writer.write(class_parser.classes_list)
+
+        dot_path = os.path.join(TESTS_DIR2, "uml0")
+        with open(dot_path, 'r') as f:
+            expected_dot = f.read()
+
+        self.assertMultiLineEqual(expected_dot, result_dot)
+
+    def test_writter_write_dot_class_with_constructor(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_class_with_constructor.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        tree = ast3.parse(code_string_list[0])
+        class_parser = ClassParser()
+        class_parser.visit(tree)
+
+        writer = DotWriter()
+        result_dot = writer.write(class_parser.classes_list)
+
+        dot_path = os.path.join(TESTS_DIR2, "uml2")
+        with open(dot_path, 'r') as f:
+            expected_dot = f.read()
+
+        self.assertMultiLineEqual(expected_dot, result_dot)
+
+    def test_writter_write_dot_class_with_constructor_data_members(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_class_with_data.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        tree = ast3.parse(code_string_list[0])
+        class_parser = ClassParser()
+        class_parser.visit(tree)
+
+        writer = DotWriter()
+        result_dot = writer.write(class_parser.classes_list)
+
+        dot_path = os.path.join(TESTS_DIR2, "uml5")
+        with open(dot_path, 'r') as f:
+            expected_dot = f.read()
+
+        self.assertMultiLineEqual(expected_dot, result_dot)
+
+    def test_writter_write_dot_class_with_constructor_data_members_methods(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_class_with_data_methods.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        tree = ast3.parse(code_string_list[0])
+        class_parser = ClassParser()
+        class_parser.visit(tree)
+
+        writer = DotWriter()
+        result_dot = writer.write(class_parser.classes_list)
+
+        dot_path = os.path.join(TESTS_DIR2, "uml1")
+        with open(dot_path, 'r') as f:
+            expected_dot = f.read()
+
+        self.assertMultiLineEqual(expected_dot, result_dot)
+
+    def test_writter_write_dot_class_classes_inheritance(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_classes_inheritance.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        tree = ast3.parse(code_string_list[0])
+        class_parser = ClassParser()
+        class_parser.visit(tree)
+
+        writer = DotWriter()
+        result_dot = writer.write(class_parser.classes_list)
+
+        dot_path = os.path.join(TESTS_DIR2, "uml3")
+        with open(dot_path, 'r') as f:
+            expected_dot = f.read()
+
+        self.assertMultiLineEqual(expected_dot, result_dot)
+
+    def test_pyuml_source_code_syntax_error(self):
+        local_dir = os.path.join(TESTS_DIR, "py_src_code_error.py")
+        loader = Loader()
+        code_string_list = loader.load_from_file_or_directory(local_dir)
+        with self.assertRaises(SyntaxError):
+            tree = ast3.parse(code_string_list[0])
+
 
 if __name__ == '__main__':
     test_dir = os.path.dirname(sys.argv[0])
     TESTS_DIR = os.path.join(test_dir, TESTS_DIR)
+    TESTS_DIR2 = os.path.join(test_dir, TESTS_DIR2)
     unittest.main()
